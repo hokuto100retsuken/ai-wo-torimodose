b6d2c6b5969c7384b765666d0fd4993495895b1a
愛内 一浩 <hokuto100retsuken@pepabo.com>
2023-03-29 10:30:14 +0900

    customerとsupportの問合せを用意する

--- src/ai-wo-torimodose.ts
+++ src/ai-wo-torimodose.ts
@@ -1,4 +1,4 @@
-import { Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
 export class AiWoTorimodose {
   openai: OpenAIApi;
@@ -10,34 +10,52 @@ export class AiWoTorimodose {
     this.openai = new OpenAIApi(configuration);
   }
 
-  revise(message: string) {
+  revise(type: "customer" | "support", message: string) {
+    let messages: ChatCompletionRequestMessage[] = [];
+
+    if (type === "customer") {
+      messages = this.customerMessages(message);
+    }
+
+    if (type === "support") {
+      messages = this.supportMessages(message);
+    }
+
     return this.openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "あなたはお問合せの文章を考えています。キーワードから文章を作成してくれます。",
-        },
-        {
-          role: "user",
-          content:
-            "以下の条件から、お問合せの文章を考えてください。パソコンが動かなくなった、昨日までは動いた、今日の14時から動かなくなった、電源ボタンを長押ししても反応がない",
-        },
-        {
-          role: "assistant",
-          content:
-            "いつもお世話になっております。私のパソコンが動かなくなってしまいました。昨日までは問題なく使用できておりましたが、今日の14時から突然動かなくなってしまいました。電源ボタンを長押ししても反応がない状態です。どうぞ、ご確認の程よろしくお願いいたします。",
-        },
-        {
-          role: "user",
-          content:
-            "以下の条件から、お問合せの文章を考えてください。丁寧な言葉を使うように表現を修正してください。" +
-            message,
-        },
-      ],
+      messages,
       temperature: 0.6,
       max_tokens: 500,
     });
+  }
+
+  private customerMessages(message: string): ChatCompletionRequestMessage[] {
+    return [
+      {
+        role: "system",
+        content:
+          "あなたはお問合せの文章を考えています。キーワードから文章を作成してくれます。",
+      },
+      {
+        role: "user",
+        content:
+          "以下の条件から、お問合せの文章を考えてください。パソコンが動かなくなった、昨日までは動いた、今日の14時から動かなくなった、電源ボタンを長押ししても反応がない",
+      },
+      {
+        role: "assistant",
+        content:
+          "いつもお世話になっております。私のパソコンが動かなくなってしまいました。昨日までは問題なく使用できておりましたが、今日の14時から突然動かなくなってしまいました。電源ボタンを長押ししても反応がない状態です。どうぞ、ご確認の程よろしくお願いいたします。",
+      },
+      {
+        role: "user",
+        content:
+          "以下の条件から、お問合せの文章を考えてください。丁寧な言葉を使うように表現を修正してください。" +
+          message,
+      },
+    ];
+  }
+
+  private supportMessages(message: string): ChatCompletionRequestMessage[] {
+    return [];
   }
 }
